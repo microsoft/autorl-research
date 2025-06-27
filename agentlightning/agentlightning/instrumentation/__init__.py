21f8d1e73cb6f1aa3d8a731c106a94a2a5e6cacd
Yuge Zhang <scotyugochang@gmail.com>
2025-06-27 06:24:12 +0000

    .

--- agentlightning/agentlightning/instrumentation/__init__.py
+++ agentlightning/agentlightning/instrumentation/__init__.py
@@ -5,19 +5,19 @@ AGENTOPS_LANGCHAIN_INSTALLED = False
 LITELLM_INSTALLED = False
 VLLM_INSTALLED = False
 
-# try:
-#     from . import agentops
+try:
+    from . import agentops
 
-#     AGENTOPS_INSTALLED = True
-# except ImportError:
-#     pass
+    AGENTOPS_INSTALLED = True
+except ImportError:
+    pass
 
-# try:
-#     from . import litellm
+try:
+    from . import litellm
 
-#     LITELLM_INSTALLED = True
-# except ImportError:
-#     pass
+    LITELLM_INSTALLED = True
+except ImportError:
+    pass
 
 # try:
 #     from . import vllm
@@ -27,40 +27,39 @@ VLLM_INSTALLED = False
 #     pass
 
 
-# try:
-#     from . import agentops_langchain
+try:
+    from . import agentops_langchain
 
-#     AGENTOPS_LANGCHAIN_INSTALLED = True
-# except ImportError:
-#     pass
+    AGENTOPS_LANGCHAIN_INSTALLED = True
+except ImportError:
+    pass
 
 
 def instrument_all():
-    pass
-    # if AGENTOPS_INSTALLED:
-    #     from .agentops import instrument_agentops
+    if AGENTOPS_INSTALLED:
+        from .agentops import instrument_agentops
 
-    #     instrument_agentops()
-    # else:
-    #     warnings.warn("agentops is not installed. It's therefore not instrumented.")
+        instrument_agentops()
+    else:
+        warnings.warn("agentops is not installed. It's therefore not instrumented.")
 
-    # if LITELLM_INSTALLED:
-    #     from .litellm import instrument_litellm
+    if LITELLM_INSTALLED:
+        from .litellm import instrument_litellm
 
-    #     instrument_litellm()
-    # else:
-    #     warnings.warn("litellm is not installed. It's therefore not instrumented.")
+        instrument_litellm()
+    else:
+        warnings.warn("litellm is not installed. It's therefore not instrumented.")
 
-    # if VLLM_INSTALLED:
-    #     from .vllm import instrument_vllm
+    if VLLM_INSTALLED:
+        from .vllm import instrument_vllm
 
-    #     instrument_vllm()
-    # else:
-    #     warnings.warn("vllm is not installed. It's therefore not instrumented.")
+        instrument_vllm()
+    else:
+        warnings.warn("vllm is not installed. It's therefore not instrumented.")
 
-    # if AGENTOPS_LANGCHAIN_INSTALLED:
-    #     from .agentops_langchain import instrument_agentops_langchain
+    if AGENTOPS_LANGCHAIN_INSTALLED:
+        from .agentops_langchain import instrument_agentops_langchain
 
-    #     instrument_agentops_langchain()
-    # else:
-    #     warnings.warn("Agentops-langchain integration is not installed. It's therefore not instrumented.")
+        instrument_agentops_langchain()
+    else:
+        warnings.warn("Agentops-langchain integration is not installed. It's therefore not instrumented.")
